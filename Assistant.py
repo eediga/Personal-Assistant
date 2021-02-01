68963d330764d497f772688ce264eda9a247d276
Eashwar <51852114+eashu554@users.noreply.github.com>
2021-02-02 01:56:25 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -1,168 +1,163 @@
-import speech_recognition as sr  # importing speech recognition package from google api
-
+import speech_recognition as sr
 import pyttsx3
-# from pygame import mixer
-import playsound    # to play saved mp3 file
-from gtts import gTTS   # google text to speech
-import os   # to save/open files
-import wolframalpha # to calculate strings into formula, its a website which provides api, 100 times per day
-from selenium import webdriver  # to control browser operations
-from selenium.webdriver.common.keys import Keys
-from io import BytesIO
-from io import StringIO
+import os
+import webbrowser
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import requests
+import json
+import geocoder
 
 
-def assistant_speaks(output):
-    global num
-    num +=1
-    print("PerSon : ", output)
-    toSpeak = gTTS(text=output, lang='en-US', slow=False)
-    file = str(num)+".mp3"
-    toSpeak.save(file)
-    # mp3_fp = BytesIO()
-    # toSpeak = gTTS(output, 'en', slow=False)
-    # toSpeak.write_to_fp(mp3_fp)
-    # os.system("D:\PeRSon\\audio\spoken.mp3")
-    '''mixer.init()
-    mixer.music.load('D:\PeRSon\\audio\spoken.mp3')
-    mixer.music.play()
-    time.sleep(5)
-    mixer.music.stop()'''
-    # song = AudioSegment.from_file(mp3_fp, format="mp3")
-    # playsound.playsound(mp3_fp)
-    playsound.playsound(file, True)
-    os.remove(file)
+g = geocoder.ip('me')
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+engine.say('Hi Eashwar')
+engine.runAndWait()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
 
-def get_audio():
-    r = sr.Recognizer()
-    audio = ''
-    with sr.Microphone() as source:
-        print("Speak...")
-        audio = r.listen(source, phrase_time_limit=5)
-    print("Stop.")
-    try:
-        text = r.recognize_google(audio,language='en-US')
-        print("You : ", text)
-        return text
-    except:
-        assistant_speaks("Could not understand your audio, PLease try again!")
-        return 0
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def weather():
+    api_url = "https://fcc-weather-api.glitch.me/api/current?lat=" + \
+              str(g.latlng[0]) + "&lon=" + str(g.latlng[1])
+
+    data = requests.get(api_url)
+    data_json = data.json()
+    if data_json['cod'] == 200:
+        main = data_json['main']
+        wind = data_json['wind']
+        weather_desc = data_json['weather'][0]
+        talk(str(data_json['coord']['lat']) + 'latitude' + str(data_json['coord']['lon']) + 'longitude')
+        talk('Current location is ' + data_json['name'] + data_json['sys']['country'] + 'dia')
+        print('Current location is ' + data_json['name'] + data_json['sys']['country'] + 'dia')
+        talk('weather type ' + weather_desc['main'])
+        talk('Wind speed is ' + str(wind['speed']) + ' metre per second')
+        talk('Temperature: ' + str(main['temp']) + 'degree celcius')
+        talk('Humidity is ' + str(main['humidity']))
 
 
-def search_web(input):
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(1)
-    driver.maximize_window()
-    if 'youtube' in input.lower():
-        assistant_speaks("Opening in youtube")
-        indx = input.lower().split().index('youtube')
-        query = input.split()[indx+1:]
-        driver.get("http://www.youtube.com/results?search_query=" + '+'.join(query))
-        return
 
-    elif 'wikipedia' in input.lower():
-        assistant_speaks("Opening Wikipedia")
-        indx = input.lower().split().index('wikipedia')
-        query = input.split()[indx + 1:]
-        driver.get("https://en.wikipedia.org/wiki/" + '_'.join(query))
-        return
-    else:
-        if 'google' in input:
-            indx = input.lower().split().index('google')
-            query = input.split()[indx + 1:]
-            driver.get("https://www.google.com/search?q=" + '+'.join(query))
-        elif 'search' in input:
-            indx = input.lower().split().index('google')
-            query = input.split()[indx + 1:]
-            driver.get("https://www.google.com/search?q=" + '+'.join(query))
-        else:
-            driver.get("https://www.google.com/search?q=" + '+'.join(input.split()))
-        return
-
-
-def open_application(input):
-    if "chrome" in input:
-        assistant_speaks("Google Chrome")
-        os.startfile('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
-        return
-    elif "firefox" in input or "mozilla" in input:
-        assistant_speaks("Opening Mozilla Firefox")
-        os.startfile('C:\Program Files\Mozilla Firefox\\firefox.exe')
-        return
-    elif "word" in input:
-        assistant_speaks("Opening Microsoft Word")
-        os.startfile('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office 2013\\Word 2013.lnk')
-        return
-    elif "excel" in input:
-        assistant_speaks("Opening Microsoft Excel")
-        os.startfile('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office 2013\\Excel 2013.lnk')
-        return
-    else:
-        assistant_speaks("Application not available")
-        return
-
-
-def process_text(input):
-    try:
-        if "who are you" in input or "define yourself" in input:
-            speak = '''Hello, I am Person. Your personal Assistant.
-            I am here to make your life easier. 
-            You can command me to perform various tasks such as calculating sums or opening applications etcetra'''
-            assistant_speaks(speak)
-            return
-        elif "who made you" in input or "created you" in input:
-            speak = "I have been created by Sheetansh Kumar."
-            assistant_speaks(speak)
-            return
-        elif "crazy" in input:
-            speak = """Well, there are 2 mental asylums in India."""
-            assistant_speaks(speak)
-            return
-        elif "calculate" in input.lower():
-            app_id= "E46YXW-T5LG6RT7K7"
-            client = wolframalpha.Client(app_id)
-
-            indx = input.lower().split().index('calculate')
-            query = input.split()[indx + 1:]
-            res = client.query(' '.join(query))
-            answer = next(res.results).text
-            assistant_speaks("The answer is " + answer)
-            return
-        elif 'open' in input:
-            open_application(input.lower())
-            return
-        elif 'search' in input or 'play' in input:
-            search_web(input.lower())
-            return
-        else:
-            assistant_speaks("I can search the web for you, Do you want to continue?")
-            ans = get_audio()
-            if 'yes' in str(ans) or 'yeah' in str(ans):
-                search_web(input)
-            else:
-                return
-    except Exception as e:
-        print(e)
-        assistant_speaks("I don't understand, I can search the web for you, Do you want to continue?")
-        ans = get_audio()
-        if 'yes' in str(ans) or 'yeah' in str(ans):
-            search_web(input)
-
-
-if __name__ == "__main__":
-    #assistant_speaks("What's your name, Human?")
-    name ='Human'
-    #name = get_audio()
-    assistant_speaks("Hello, " + name + '.')
-    while(1):
-        assistant_speaks("What can i do for you?")
-        assistant_speaks("i am your personal assistant")
-        text = get_audio().lower()
-        if text == 0:
-            continue
-        #assistant_speaks(text)
-        if "exit" in str(text) or "bye" in str(text) or "go " in str(text) or "sleep" in str(text):
-            assistant_speaks("Ok bye, "+ name+'.')
+def talk_news():
+    url = ('http://newsapi.org/v2/top-headlines?'
+           'sources=bbc-news&'
+           'apiKey=10790965ee1541dd96354d461fb2b65f')
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    talk('Eashwar the Source is: The Times Of India')
+    talk('Todays Headlines are..')
+    for index, articles in enumerate(arts):
+        talk(articles['title'])
+        print(articles['title'])
+        if index == len(arts)-1:
             break
-        process_text(text)
+        talk('Moving on the next news headline..')
+    talk('These were the top headlines, Have a nice day Sir!!..')
+
+def getNewsUrl():
+    return 'http://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=9b17ea366f744f5eadbbd8dc545e3f79'
+
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('eashwar please wait while the song is playing ' + song )
+        pywhatkit.playonyt(song)
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Eashwar current time is ' + time)
+    elif 'date' in command:
+        year = int(datetime.datetime.now().year)
+        month = int(datetime.datetime.now().month)
+        date = int(datetime.datetime.now().day)
+        talk("the current date is")
+        talk(date)
+        talk(month)
+        talk(year)
+        print(date, month, year)
+
+
+
+    elif 'open youtube' in command:
+        webbrowser.open("www.youtube.com")
+        talk('Eashwar please wait while i am  opening youtube')
+    elif 'open facebook' in command:
+        webbrowser.open("www.facebook.com")
+        talk('Eashwar please wait while i am  opening facebook')
+    elif 'open google' in  command:
+        talk('Eashwar what can i search in google')
+        cm = take_command().lower()
+        webbrowser.open(f"{cm}")
+        talk('Eashwar please wait while i am  opening google')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'weather' in command:
+        weather()
+    elif 'Hi'  in command:
+        talk('Thank you eashwar i am fine how may i help you?')
+    elif 'news' in command:
+        talk('Ofcourse Eashwar..')
+        talk_news()
+        talk('Eashwar Do you want to read the full news...')
+        test = take_command()
+        if 'yes' in test:
+            talk('Ok Eashwar, Opening browser...')
+            webbrowser.open(getNewsUrl())
+            talk('You can now read the full news from this website.')
+        else:
+            talk('No Problem Eashwar')
+
+
+    elif 'how are you' in command:
+        talk('Thank you eashwar i am fine how may i help you?')
+
+    if 'jarvis are you there' in command:
+        talk("Yes Eashwar, at your service")
+
+
+    elif 'open notepad' in command:
+        npath = "C:\\WINDOWS\\system32\\notepad.exe"
+        os.startfile(npath)
+        talk('Eashwar please wait while i am  opening notepad')
+    elif 'open command prompt' in command:
+        os.system("start cmd")
+    elif 'who is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'close notepad' in  command:
+        os.system("taskkill /f /im notepad.exe")
+        talk('Eashwar i have closed notepad')
+
+
+while True:
+    run_alexa()
+
